36e615079ae61d8927a8841cd35ea6f62b980f10
errrrvb <roman-vavilov123region@yandex.ru>
2023-02-25 23:29:15 +0300

    done

---
+++ 6.py
@@ -0,0 +1,13 @@
+a = int(input())  # ввод переменных
+b = int(input())
+c = int(input())
+
+d = b*b+4*a*c
+if d < 0:
+    print("")
+elif d == 0:
+    print(-b/2*a)
+else:
+    x1 = (-b + d ** 0.5) / (2*a)
+    x2 = (-b - d ** 0.5) / (2*a)
+    print(x1,x2)
